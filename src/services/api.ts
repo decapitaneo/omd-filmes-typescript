827dbe365bde351c976c98b9065d9c562cf8b145
Denise Capitaneo <decapitaneo@gmail.com>
2023-07-16 22:08:34 -0300

    ajuste url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://www.omdbapi.com/?apikey=f6ce0a28&'
+    baseURL: 'https://www.omdbapi.com/?apikey=f6ce0a28&'
 });
 
 export default api
